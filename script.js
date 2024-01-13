1e5b3583fa88097824e10290a5914e055b7813d2
PranshDalal <121412920+PranshDalal@users.noreply.github.com>
2024-01-12 17:22:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // You can add your logic to handle the form submission here
-    // For example, fetching data with AJAX or sending it to a server
-    // For now, let's log the form data to the console
     const formData = new FormData(event.target);
     for (let [name, value] of formData) {
         console.log(`${name}: ${value}`);
